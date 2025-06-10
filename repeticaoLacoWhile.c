9756e8ccc563aeaf0ad910ba599423b0e1d1a231
IgorRRamos <ramosigorprofissional@gmail.com>
2025-06-09 22:31:04 -0300

    Estudo laco de repeticao while

---
+++ repeticaoLacoWhile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int num=0;
+
+    while(num < 20){
+
+        printf("Por favor informe um numero: ");
+        scanf("%i", &num);
+
+    }
+    printf("Fim do programa!\n");
+
+    return 0;
+}
