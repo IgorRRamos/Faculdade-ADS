33ecdc2f67a3737019ab799f59d4652565eb769c
IgorRRamos <ramosigorprofissional@gmail.com>
2026-01-26 16:10:12 -0300

    Adicionando comentaios

---
+++ Java/javaComments/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //Isso e um comentario em java
+        System.out.println("Hello World!");
+
+
+    }
+}
