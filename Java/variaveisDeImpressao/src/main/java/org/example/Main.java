1e1f66317f0d2c327b21cf1283eba333a8a92d26
IgorRRamos <ramosigorprofissional@gmail.com>
2026-01-26 16:20:18 -0300

    Vaiaveis de impessao

---
+++ Java/variaveisDeImpressao/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Para Stirng o (+) une as stirng, chamamos de concatenaçao.
+        String firstName = "Igor ";
+        String lastName = "Ramos";
+
+        String fullName = firstName + lastName;
+
+        System.out.println(fullName);
+
+        //Para numeros o (+) funciona como opeador aritimetico.
+        int num1 = 5;
+        int num2 = 10;
+
+        int soma = num1 + num2;
+
+        System.out.println(soma);
+
+
+    }
+}
