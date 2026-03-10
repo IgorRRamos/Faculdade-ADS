08d2bd6f391e0827bb294cd669e034b56b95e49a
IgorRRamos <ramosigorprofissional@gmail.com>
2026-03-10 19:47:17 -0300

    Teste repositorio

---
+++ Java/testeRetorno/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 10;
+        int resultado = somar(num1, num2);
+
+        System.out.println(resultado);
+    }
+
+    public static int somar(int num1, int num2) {
+        return num1 + num2;
+    }
+}
